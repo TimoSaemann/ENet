427a01442c0998bb314751e39c7c386aab819129
Timo Sämann <Timo.Saemann@gmx.de>
2017-07-11 18:25:33 +0200

    Update enet_weigths_zoo with 'cityscapes_weights_before_bn_merge.caffemodel'

--- enet_weights_zoo/cityscapes_weights.sh
+++ enet_weights_zoo/cityscapes_weights.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 wget -O $DIR/weights.caffemodel https://www.dropbox.com/sh/5wjmb5twfr5b0wo/AADEpW5a8-GSSt5pfJYCKxoOa?dl=1
+wget -O $DIR/weights2.caffemodel https://www.dropbox.com/s/3i367gsl7sspeo1/cityscapes_weights_before_bn_merge.caffemodel?dl=1
 unzip $DIR/weights.caffemodel 
+unzip $DIR/weights2.caffemodel 
 rm $DIR/weights.caffemodel
+rm $DIR/weights2.caffemodel
+
